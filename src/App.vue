60aebf22b8e88700e1188b4a5232ae0cda5d7cd9
Juan Chemello <juanchemell@gmail.com>
2020-07-05 17:54:57 -0300

    base-todo-lists

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <product-list-one :products="products"></product-list-one>
+    <product-list-two :products="products"></product-list-two>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import productOne from './components/product-list-one'
+import productTwo from './components/product-list-two'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      products: [
+        { name: 'Banana Skin', price: 20 },
+        { name: 'Shiny  Stars', price: 40 },
+        { name: 'Green Shells', price: 60 },
+        { name: 'Red Shells', price: 80 }
+      ]
+    }
+  },
   components: {
-    HelloWorld
+    'product-list-one': productOne,
+    'product-list-two': productTwo
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  font-family: Nunito;
+  color: #555;
 }
 </style>
